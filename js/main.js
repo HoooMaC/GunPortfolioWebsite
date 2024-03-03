bb43383eb44eafd952056f7240a3c2394866f76b
HoooMaC <wawcam123@gmail.com>
2024-03-03 22:40:41 +0700

    remaking website and making the navbar
    using vite

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import '../styles/modern-normalize.css'
+
+import '../styles/components/navbar.css'
+import '../styles/style.css'
+
+import '../styles/utils.css'
+
+
+const sections = document.querySelectorAll('section[id]');
+const navbarLinks = document.querySelectorAll('.navbar__menu a');
+
+window.onscroll = () => {
+    sections.forEach(current => {
+        let top = window.scrollY;
+        let offset = current.offsetTop;
+        let height = current.offsetHeight;
+        let id = current.getAttribute('id');
+
+        if(top >= offset && top < offset + height)
+        {
+            navbarLinks.forEach(link => {
+                link.classList.remove('active-link');
+                document.querySelector('.navbar__menu a[href*=' + id +']').classList.add('active-link');
+            });
+        }
+    })
+}
